96cd82a64cb615edc2a64b9529fd755bc1a152c4
Vuong Ho <hochivuong2002@gmail.com>
2021-05-10 13:55:30 +0700

    Add getting help for specific command

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,8 @@ module.exports = {
 	execute(message, args) {
 		const data = [];
 		const { commands } = message.client;
-        // TODO: add support for specific commands
+
+        // Get list of every commands
 		if (!args.length) {
             var embed = new Discord.MessageEmbed()
                 .setTitle('Here\'s the list of available commands')
@@ -15,10 +16,29 @@ module.exports = {
             for(var [key, value] of commands.entries()) {
                 embed.addField(prefix + key, value.description);
             }
-        
             return message.channel.send(embed)
 		}
+
+        // Search for specific command
+        for(var [key, value] of commands.entries()) {
+            if(key.localeCompare(args[0]) == 0) {
+                return sendEmbedHelp(message, key, value.description);
+            }
+        }
+
+        // If find no command
+        message.channel.send(`Oopsie, there\'s no command \`${args[0]}\` that I know of.`)
 	},
 };
 
 /////////////////////////////MAIN FUNCTION/////////////////////////////
+
+
+/////////////////////////////FUNCTIONS BELOW/////////////////////////////
+function sendEmbedHelp(message, name, description) {
+    var embed = new Discord.MessageEmbed()
+        .setTitle('Here\'s info for your requested command')
+        .setColor(0xff0000)
+        .addField(prefix + name, description);
+    return message.channel.send(embed);
+}
