2e1a3a5720676d4d8d199a7bdc0d181699e14234
Vuong Ho <hochivuong2002@gmail.com>
2021-05-12 13:21:34 +0700

    Add error handling

---
+++ error/error.js
@@ -0,0 +1,10 @@
+module.exports = {
+    sendError: function(message) {
+        message.reply('Whoopsie doopsie, there\'s a problem requesting search. Please tell this to the simp that created me.');
+        message.reply('オットー、エラーか。これを作成したSIMPに報告してくださいね、フヒヒ。');
+    },
+    sendErrorCode: function(message, errorCode) {
+        message.reply(`Whoopsie doopsie, there\'s a problem. Please tell this to the simp that created me. Error code ${ errorcode }`);
+        message.reply(`オットー、エラーか。これを作成したSIMPに報告してくださいね、フヒヒ。エラーコード ${ errorCode }`);        
+    }
+}
