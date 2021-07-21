26e7986046d3797883c174832a722e9215832e4b
Vuong Ho <hochivuong2002@gmail.com>
2021-07-21 20:03:18 +0700

    Small refactor in timetable command

--- commands/main/timetable.js
+++ commands/main/timetable.js
@@ -23,35 +23,7 @@ module.exports = {
                 return message.channel.send('Timetable refreshed!');
             case 'saya':
             case 'sayacchi':
-                // TODO: check if timetable is too old by getting time from somewhere
-                message.channel.send('You want to know what time I\'m live?');
-                var timeslotsToday = checkLiveInTimetable('hiyama', getStoredTodayTimetable());
-                var timeslotsTomorrow = checkLiveInTimetable('hiyama', getStoredTomorrowTimetable());
-                var errorStopper = 0;
-                while(typeof timeslotsToday === 'undefined') {
-                    message.channel.send('Too bad there\'s nothing on my current timetable, I\'ll refresh my timetable for you!');
-                    await refreshTimetable();
-                    timeslotsToday = checkLiveInTimetable('hiyama', getStoredTodayTimetable());
-                    timeslotsTomorrow = checkLiveInTimetable('hiyama', getStoredTomorrowTimetable());
-                    // In case of infinite while loop, return and send error code 1
-                    if(errorStopper == 5) return Error.sendErrorCode(message, 1);
-                    errorStopper++;
-                }
-
-                var day = 0; // 0 = today, 1 = tomorrow
-                while(day <= 1) {
-                    var timeslots = (day == 0) ? timeslotsToday : timeslotsTomorrow;
-                    var dayText = (day == 0) ? 'today' : 'tomorrow';
-                    if(timeslots.length == 0) {
-                        message.channel.send(`Too bad I\'ll not be live ${ dayText }, but you can try refreshing by doing \`!timetable refresh\` to see if thing\'s better!`);
-                    } else {
-                        message.channel.send(`Here\'s when I will be live ${ dayText }:`);
-                        for (var timeslot of timeslots) {
-                            sendTimeslot(message, timeslot);
-                        }
-                    }
-                    day++;
-                }
+                await checkLiveCommand(message);
                 break;
             case 'today': // TODO:
                 message.channel.send('You want to know the timetable for today?');
@@ -153,10 +125,10 @@ async function retrieveTimetable() {
 }
 
 /*  
-    Send WNI timetable data from storage, if nothing in storage then refresh
-    limit:  0 => send all
-            1 => send today's
-            2 => send tomorrow's (not implemented yet)
+ *  Send WNI timetable data from storage, if nothing in storage then refresh
+ *  limit:  0 => send all
+ *          1 => send today's
+ *          2 => send tomorrow's (not implemented yet)
  */
 async function sendTimetable(message, limit) {
     message.channel.send("Here's your timetable!");
@@ -223,10 +195,45 @@ function sendTimeslot(message, timeslot) {
     message.channel.send(embed);
 }
 
+/**
+ * Helper function for check live command
+ */
+async function checkLiveCommand(message) {
+    // TODO: check if timetable is too old by getting time from somewhere
+    message.channel.send('You want to know what time I\'m live?');
+    var timeslotsToday = checkLiveInTimetable('hiyama', getStoredTodayTimetable());
+    var timeslotsTomorrow = checkLiveInTimetable('hiyama', getStoredTomorrowTimetable());
+    var errorStopper = 0;
+    while(typeof timeslotsToday === 'undefined') {
+        message.channel.send('Too bad there\'s nothing on my current timetable, I\'ll refresh my timetable for you!');
+        await refreshTimetable();
+        timeslotsToday = checkLiveInTimetable('hiyama', getStoredTodayTimetable());
+        timeslotsTomorrow = checkLiveInTimetable('hiyama', getStoredTomorrowTimetable());
+        // In case of infinite while loop, return and send error code 1
+        if(errorStopper == 5) return Error.sendErrorCode(message, 1);
+        errorStopper++;
+    }
+
+    var day = 0; // 0 = today, 1 = tomorrow
+    while(day <= 1) {
+        var timeslots = (day == 0) ? timeslotsToday : timeslotsTomorrow;
+        var dayText = (day == 0) ? 'today' : 'tomorrow';
+        if(timeslots.length == 0) {
+            message.channel.send(`Too bad I\'ll not be live ${ dayText }, but you can try refreshing by doing \`!timetable refresh\` to see if thing\'s better!`);
+        } else {
+            message.channel.send(`Here\'s when I will be live ${ dayText }:`);
+            for (var timeslot of timeslots) {
+                sendTimeslot(message, timeslot);
+            }
+        }
+        day++;
+    }
+}
+
 /* 
-    Check if input caster name has live time slots in timetable in storage
-    return array of timeslot for caster if input caster name is live,
-    undefined else
+ *  Check if input caster name has live time slots in timetable in storage
+ *  return array of timeslot for caster if input caster name is live,
+ *  undefined else
  */
 function checkLiveInTimetable(casterName, timetable) {
     if(typeof timetable === 'undefined') return undefined; 
@@ -243,7 +250,7 @@ function checkLiveInTimetable(casterName, timetable) {
 }
 
 /* 
-    Check if input caster name is currently live in today's timetable
+ *  Check if input caster name is currently live in today's timetable
  */
 function checkLiveWithCurrentTime(casterName) {
     var timeslots = checkLiveInTimetable(casterName, this.todayTimetable);
