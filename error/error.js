b9a6f9c708e35c6ee7fb1b8d4182c5809212de9b
Vuong Ho <hochivuong2002@gmail.com>
2021-07-26 20:27:16 +0700

    Add send to channel error reporting option

--- error/error.js
+++ error/error.js
@@ -6,5 +6,9 @@ module.exports = {
     sendErrorCode: function(message, errorCode) {
         message.reply(`Whoopsie doopsie, there\'s a problem. Please tell this to the simp that created me. Error code ${ errorCode }`);
         message.reply(`オットー、エラーか。私を作成したSIMPに報告してくださいね、フヒヒ。エラーコード ${ errorCode }`);        
+    },
+    sendErrorCodeToChannel: function (channel, errorCode) {
+        channel.send(`Whoopsie doopsie, there\'s a problem. Please tell this to the simp that created me. Error code ${ errorCode }`);
+        channel.send(`オットー、エラーか。私を作成したSIMPに報告してくださいね、フヒヒ。エラーコード ${ errorCode }`);
     }
 }
